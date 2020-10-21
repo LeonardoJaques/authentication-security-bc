651a294cbaf472aca30057f70b8415ddfd62c2cc
LeonardoJaques <jaques.projetos@outlook.com>
2020-10-21 15:35:25 -0300

    Level 1 - Register Users with Username and Password

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //jshint esversion:6
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -11,6 +13,18 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -19,6 +33,44 @@ app.get("/login", (req, res) => {
 });
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+
+app.post("/register", (req, res) => {
+
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save((err) => {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  }, (err, foundUser) => {
+    if (!err) {
+      //this ugly code O(n^3)
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    } else {
+      console.log(err);
+    }
+  })
+
 });
 
 const port = process.env.PORT || 3000
