cf4d32d830479ea31ba7c7bf4e16499950096a55
LeonardoJaques <jaques.projetos@outlook.com>
2020-10-21 16:38:40 -0300

    Level 2 - Database Encryption

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+var encrypt = require('mongoose-encryption');
+
 
 const app = express();
 
@@ -18,10 +20,17 @@ mongoose.connect('mongodb://localhost:27017/userDB', {
   useUnifiedTopology: true
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecreat.";
+
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ["password"]
+});
 
 const User = new mongoose.model("User", userSchema);
 
